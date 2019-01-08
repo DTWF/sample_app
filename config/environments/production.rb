ced389069ed9485abfdd62877d4c9bf2a32c3e0a
Dushko Tasevski <dushkotasevski@webfactory.mk>
2019-01-08 16:58:39 +0100

    Add production email service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,6 @@
+require 'rest_client'
+require 'json'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -71,14 +74,18 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = "https://lit-atoll-91449.herokuapp.com/"
   config.action_mailer.default_url_options = {host: host}
+  response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+
+  first_inbox = JSON.parse(response)[0] # get first inbox
+
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+      :user_name => first_inbox['username'],
+      :password => first_inbox['password'],
+      :address => first_inbox['domain'],
+      :domain => first_inbox['domain'],
+      :port => first_inbox['smtp_ports'][0],
+      :authentication => :plain
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
